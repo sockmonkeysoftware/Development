0650ec939c6094631d2b0d6d465301ec144a06aa
Jordan Adamek <jadamek2@kent.edu>
2016-03-20 05:59:20 +0000

    Update route implemented

--- src/Controller/BoardController.py
+++ src/Controller/BoardController.py
@@ -1,14 +1,19 @@
-import sys
-sys.path.append("/home/jadamek/Sudoku/Development/src/Model")
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../Model")
 
 from bottle import static_file, template
 from Board import Board
 import json
 
+# Make this a Session variable.
 game_board = None
 
+#----------------------------------------------------------------
 def play():
-	game_board = Board(40)
+#----------------------------------------------------------------
+# Creates a new sudoku board and returns a template displaying it
+#----------------------------------------------------------------
+	game_board = Board(40, False)
 	
 	raw_map = game_board.getRaw()
 	board_map = {}
@@ -24,8 +29,28 @@ def play():
 
 	return template('index', board = board_map)
 
+#----------------------------------------------------------------
 def update(cell_id, value):
-	pass
+#----------------------------------------------------------------
+# * cell_id : the character-int pair (row-column) where the guess
+#	was made.
+# * value : the user's guess (1~9) for the cell's solution.
+# Submits a user's input to the board.
+# Returns True if the guess was correct, and False otherwise.
+#----------------------------------------------------------------
+	if game_board is not None:
+		# Translate coordinates back to 2-D array.
+		x = int(cell_id[1]) - 1
+		y = ord(cell_id[0]) - ord('a')
 
+		answer = int(value)
+		
+		# Submit the user's answer to the board.
+		correct = game_board.submitAnswer(x, y, answer)
+
+		return correct
+
+#----------------------------------------------------------------
 def status():
+#----------------------------------------------------------------
 	pass
